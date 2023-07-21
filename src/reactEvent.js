2ad9e17a2071e7065890ea5b44d1ac3c5a8c855e
Saurav Rana <sauravranawins@gmail.com>
2023-07-21 17:31:30 +0530

    Using all Event Method

--- src/reactEvent.js
+++ src/reactEvent.js
@@ -1,22 +1,25 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import "./css/Common.css";
 
+//implement all diffrent method of event handling
 const ReactEvent = () => {
-  let [ name, setname] = useState("saurav");
+  let [name, setname] = useState("saurav");
 
   const clickbtn = (value) => {
-    setname(value)
-    // document.getElementsByClassName("button").style.display="show";
+    setname(value);
   };
+
+  //   const changevalue = ()=>{
+  //     clickbtn("Shweta")
+  //   }
 
   return (
     <div className="box">
       <h1>React Event</h1>
       <button onClick={() => clickbtn("Shweta")}>click me</button>
+      {/* <button onClick={clickbtn.bind(this,"Shweta")}>click me</button> */}
+      {/* <button onClick={changevalue}>click me</button> */}
       <h1>{name}</h1>
-      {/* <h1 id="person" className="button">
-        {name}
-      </h1> */}
     </div>
   );
 };
